1fdfd2f886d308124069110560b5bb07af80fe6a
MD Ripon Islam <islamripon876@gmail.com>
2015-11-18 18:46:31 +0600

    initial commit

---
+++ app.js
@@ -0,0 +1,16 @@
+// var express = require("express");
+// var app = express();
+
+// app.get("/",function(req,res){
+// 	res.send("this is the home route");
+// });
+
+// app.listen("3000",function(){
+// 	console.log("Server started at port 3000...");
+// });
+
+console.log("===========================")
+console.log(" ")
+console.log("Welcome to this app")
+console.log(" ")
+console.log("===========================")
