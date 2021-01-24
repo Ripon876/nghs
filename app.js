89460886884977161f183eb60b4a4206e6e30cc2
MD Ripon Islam <islamripon876@gmail.com>
2021-01-24 19:28:19 +0600

    Added modules

--- app.js
+++ app.js
@@ -1,16 +1,10 @@
-// var express = require("express");
-// var app = express();
+var express = require("express");
+var app = express();
+var port = process.env.PORT || 8080;
+app.get("/",function(req,res){
+	res.send("this is the home route");
+});
 
-// app.get("/",function(req,res){
-// 	res.send("this is the home route");
-// });
-
-// app.listen("3000",function(){
-// 	console.log("Server started at port 3000...");
-// });
-
-console.log("===========================")
-console.log(" ")
-console.log("Welcome to this app")
-console.log(" ")
-console.log("===========================")
+app.listen(port,function(){
+	console.log("Server started at port " + port);
+});
